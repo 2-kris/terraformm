a810a2b57d11a1faae6358126ac6fd03aeabeec3
2-kris <misaalkrishna@gmail.com>
2026-03-17 11:02:06 +0530

    new changes

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -15,6 +15,7 @@ resource "aws_key_pair" "my-key" {
 resource "aws_subnet" "public-subnet-1" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-1"
